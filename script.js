761cce0cd67d48b710594639a45077dbe336a5ce
N270203MR <maldonoa@gmail.com>
2025-07-31 13:56:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-let dades = []
+let dades = {};
 
 document.addEventListener("DOMContentLoaded", function () {
-  const gens = Object.keys(dades);
-  omplirSelect('gen1', gens);
-  omplirSelect('gen2', gens);
+  fetch('dades.json')
+    .then(response => response.json())
+    .then(json => {
+      dades = json;
+      const gens = Object.keys(dades);
+      omplirSelect('gen1', gens);
+      omplirSelect('gen2', gens);
 
-  ['gen1', 'gen2'].forEach(genId => {
-    document.getElementById(genId).addEventListener('change', (e) => {
-      const fenotipId = genId === 'gen1' ? 'fenotip1' : 'fenotip2';
-      const val = e.target.value;
-      if (val && dades[val]) {
-        omplirSelect(fenotipId, Object.keys(dades[val]));
-      } else {
-        document.getElementById(fenotipId).innerHTML = '<option value="">-- Selecciona --</option>';
-      }
+      ['gen1', 'gen2'].forEach(genId => {
+        document.getElementById(genId).addEventListener('change', (e) => {
+          const fenotipId = genId === 'gen1' ? 'fenotip1' : 'fenotip2';
+          const val = e.target.value;
+          if (val && dades[val]) {
+            omplirSelect(fenotipId, Object.keys(dades[val]));
+          } else {
+            document.getElementById(fenotipId).innerHTML = '<option value="">-- Selecciona --</option>';
+          }
+        });
+      });
     });
-  });
 });
 
 function omplirSelect(id, opcions) {
