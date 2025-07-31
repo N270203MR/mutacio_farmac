efb1a6462a42785647874105996a59c90db32b62
N270203MR <maldonoa@gmail.com>
2025-07-31 13:18:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,26 @@ function descarregarPDF() {
   doc.text(text, 10, 10);
   doc.save('recomanacio_farmacogenetica.pdf');
 }
+document.addEventListener("DOMContentLoaded", function () {
+  fetch('dades.json')
+    .then(response => response.json())
+    .then(json => {
+      dades = json;
+      const gens = Object.keys(dades);
+      omplirSelect('gen1', gens);
+      omplirSelect('gen2', gens);
+    });
+
+  ['gen1', 'gen2'].forEach(genId => {
+    document.getElementById(genId).addEventListener('change', (e) => {
+      const fenotipId = genId === 'gen1' ? 'fenotip1' : 'fenotip2';
+      const val = e.target.value;
+      if (val && dades[val]) {
+        omplirSelect(fenotipId, Object.keys(dades[val]));
+      } else {
+        document.getElementById(fenotipId).innerHTML = '<option value="">-- Selecciona --</option>';
+      }
+    });
+  });
+})
+
